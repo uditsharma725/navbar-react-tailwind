1a809a5914b8dbc87f2a355e3908cfd21932eaf1
uditsharma725 <72641703+uditsharma725@users.noreply.github.com>
2022-11-23 08:49:55 +0530

    starting navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+      <div className='bg-white'>
+        <nav className=''>
+            <div className='name'></div>
+            <ul className='list'></ul>
+        </nav>
+      </div>
+    </>
+  )
+}
